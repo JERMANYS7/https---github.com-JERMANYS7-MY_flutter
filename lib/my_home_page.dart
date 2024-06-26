bbc46f94c9563d7b04c8fdacee43d09b4f2eb1a0
boyb5558 <boyb5558@gmail.com>
2024-06-26 05:07:34 +0000

    test commit

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -10,40 +10,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
- 
-      _counter*=2;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-  
-        title: Text(widget.title),
+        title: const Text("Hello, My Friend"),
       ),
-      body: Center(
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            
-            
-          ],
-        ),
+    body: const Center(
+      child: Column(
+        children: [
+          Text("data"),
+          Text("data"),
+          Text("data"),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
